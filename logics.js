63b917f7034c4d6b7da278ab0c5ac5f36d0fc8f7
Jeremy Bouchard <JBouchard1134@gmail.com>
2020-08-08 00:54:09 -0700

    first commit

---
+++ logics.js
@@ -0,0 +1,36 @@
+const $answers = document.querySelector('#answers');
+const questions = [
+    {
+        question: 'What is the answer?',
+        answer: [
+            {text:'Answer 1', correct: true},
+            {text:'Answer 2', correct: false},
+            {text:'Answer 3', correct: false},
+            {text:'Answer 4', correct: false}
+        ]
+    },
+
+    {
+        question: 'What is the answer?',
+        answer: [
+            {text:'Answer 1', correct: true},
+            {text:'Answer 2', correct: false},
+            {text:'Answer 3', correct: false},
+            {text:'Answer 4', correct: false}
+        ]
+    },
+
+    {
+        question: 'What is the answer?',
+        answer: [
+            {text:'Answer 1', correct: true},
+            {text:'Answer 2', correct: false},
+            {text:'Answer 3', correct: false},
+            {text:'Answer 4', correct: false}
+        ]
+    },
+]
+
+$answers.addEventListener('click', function() {
+    
+});
