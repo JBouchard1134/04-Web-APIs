94a4b70a0ba35a430a1ea7a5dbce775430ed908e
Jeremy Bouchard <JBouchard1134@gmail.com>
2020-08-09 14:59:32 -0700

    Updated work with working buttons and timer.

--- logics.js
+++ logics.js
@@ -1,36 +1,89 @@
 const $answers = document.querySelector('#answers');
+const $startButton = document.getElementById('start-button');
+const $question = document.getElementById('question');
+const $questionSection = document.getElementById('question-section');
+
+let $timer = document.getElementById('timer');
+let currentQuestion = 0;
+let secondsLeft = 150;
+
+
 const questions = [
     {
-        question: 'What is the answer?',
+        question: 'What is a boolean?',
         answer: [
-            {text:'Answer 1', correct: true},
-            {text:'Answer 2', correct: false},
-            {text:'Answer 3', correct: false},
-            {text:'Answer 4', correct: false}
-        ]
+            {text:'A demon God'},
+            {text:'A true or false value'},
+            {text:'an HTML tag'},
+            {text:'whatever'}
+        ],
+        correctAnswer: 'A true or false value',
     },
 
     {
-        question: 'What is the answer?',
+        question: 'What is the answerA?',
         answer: [
-            {text:'Answer 1', correct: true},
-            {text:'Answer 2', correct: false},
-            {text:'Answer 3', correct: false},
-            {text:'Answer 4', correct: false}
-        ]
+            {text:'Answer A', correct: true},
+            {text:'Answer B', correct: false},
+            {text:'Answer C', correct: false},
+            {text:'Answer D', correct: false}
+        ],
+        correctAnswer: 'Answer C',
     },
 
     {
-        question: 'What is the answer?',
+        question: 'What is the answerB?',
         answer: [
-            {text:'Answer 1', correct: true},
-            {text:'Answer 2', correct: false},
-            {text:'Answer 3', correct: false},
-            {text:'Answer 4', correct: false}
-        ]
+            {text:'Answer 11', correct: true},
+            {text:'Answer 22', correct: false},
+            {text:'Answer 33', correct: false},
+            {text:'Answer 44', correct: false}
+        ],
+        correctAnswer: 'Answer 44',
     },
 ]
 
-$answers.addEventListener('click', function() {
+
+function displayQuestions() {
+    $question.textContent = questions[currentQuestion].question;
+
+    for(var i = 0; i < 4; i++) {
+        document.getElementById('btn' + i).innerText = questions[currentQuestion].answer[i].text;
+    }
     
+}
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      $timer.textContent = secondsLeft + " seconds left till the whole world collapses";
+  
+      if(secondsLeft <= 0) {
+        clearInterval(timerInterval);
+        alert('Out of time!');
+      }
+  
+    }, 1000);
+  }
+
+$startButton.addEventListener('click', function() {
+    $startButton.style.display = 'none';
+    $questionSection.style.display = 'block';  
+    setTime();
+    displayQuestions();
+})
+
+$answers.addEventListener('click', function(event) {
+    if (event.target.nodeName !==  'BUTTON') {
+        return;
+    }
+    console.log(event.target.nodeName);
+
+    let userAnswer = event.target.innerText;
+    
+    if (userAnswer !== questions[currentQuestion].correctAnswer) {
+        secondsLeft = secondsLeft  - 10;
+    }
+
+
 });
